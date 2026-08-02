e98337cb72968ee81fbb07183b62581e5707eb39
Niko Kozak <nikokozak@gmail.com>
2026-08-01 20:38:04 -0400

    fix(tests): include thread tool receipt handler

--- Tests/TickerTests/StreamDocumentTests.swift
+++ Tests/TickerTests/StreamDocumentTests.swift
@@ -2704,6 +2704,7 @@ final class StreamDocumentTests: XCTestCase {
                 "createStreamThread",
                 "deleteStreamThread",
                 "loadStreamThread",
+                "recordThreadToolApplication",
                 "removeStreamThreadAnchor",
                 "saveStreamThread"
             ])
